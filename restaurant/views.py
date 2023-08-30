16165f18d4a18b4d0b106d71ea16091407bc21a0
keyxcode <keyxcode@gmail.com>
2023-08-30 13:15:03 +0700

    Add index page template

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
 # Create your views here.
-@api_view(["GET"])
-def hello_world(request):
-    return Response("hello")
+def index(request):
+    return render(request, "index.html", {})
